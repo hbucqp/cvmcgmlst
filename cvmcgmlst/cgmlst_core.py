79b384514f54dfdc530db7f90d228683fca39139
崔箐坡 <hbucqp1991@sina.cn>
2023-03-22 12:20:50 +0800

    solve Bio.Application.ApplicationEorror: Non-Zero code 1

--- cvmcgmlst/cgmlst_core.py
+++ cvmcgmlst/cgmlst_core.py
@@ -33,7 +33,7 @@ class mlst():
         #                               num_threads=self.threads)
         cline = NcbiblastnCommandline(query=self.inputfile, db=self.database, dust='no', ungapped=True,
                                       evalue=1E-20, out=self.temp_output,  # delete culling_limit parameters
-                                      outfmt="6 sseqid slen length nident",
+                                      outfmt='"6 sseqid slen length nident"',
                                       # task='dc-megablast',
                                       perc_identity=self.minid, max_target_seqs=1000000,
                                       num_threads=self.threads)
@@ -58,15 +58,17 @@ class mlst():
                 # resolve the bug that could not get exactly matched allele
                 if hlen == alen & nident == hlen:  # exact match
                     if gene in result.keys():
-                        
+
                         if not re.search(r'[~\?]', str(result[gene])):
-                            old_num = int(result[gene] )                          
+                            old_num = int(result[gene])
                             if num < old_num:
                                 # print(f'{num}\t{old_num}')
                                 result[gene] = num
-                                print(f'Found additional allele match, replace {gene}:{old_num} -> {num}')
+                                print(
+                                    f'Found additional allele match, replace {gene}:{old_num} -> {num}')
                             else:
-                                print(f'Found additional allele match, but the allele number {num} is greater or equal to stored one {gene}:{old_num}, skip...')
+                                print(
+                                    f'Found additional allele match, but the allele number {num} is greater or equal to stored one {gene}:{old_num}, skip...')
                         else:  # replace not perfect match
                             result[gene] = num
                     else:

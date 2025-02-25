bbb14845a056585faede8d549615eda0a65f47cc
崔箐坡 <hbucqp1991@sina.cn>
2025-02-25 21:54:31 +0800

    update

--- setup.py
+++ setup.py
@@ -18,24 +18,13 @@ except ImportError:
 
 
 requirements = [
-    'Bio', 'pandas', 'setuptools'
+    'Bio',
+    'pandas',
+    'setuptools',
+    'cvmcore >= 0.2.2',
+    'cvmblaster >= 0.4.8',
+    'tabulate'
 ]
-
-
-def read(rel_path: str) -> str:
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with open(os.path.join(here, rel_path)) as fp:
-        return fp.read()
-
-
-def get_version(rel_path: str) -> str:
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
 
 
 about = {}
@@ -57,27 +46,26 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     license="MIT Licence",
-    url="https://github.com/hbucqp/cvmcgmlst",
+    url=about['__url__'],
     author=about['__author__'],
     author_email=about['__author_email__'],
     packages=find_packages(),
-    include_package_data=True,
-    # package_data={'': ['*']},
+    include_package_data=True,  # Ensure package data is included
     platforms="any",
     install_requires=requirements,
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         # Define that your audience are developers
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         # Specify which pyhton versions that you want to support
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points={
         'console_scripts': [

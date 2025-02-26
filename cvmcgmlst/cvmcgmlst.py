40e8c5381f95e02e23d36946dde8892a51a7f458
崔箐坡 <hbucqp1991@sina.cn>
2025-02-26 11:27:31 +0800

    update main function

--- cvmcgmlst/cvmcgmlst.py
+++ cvmcgmlst/cvmcgmlst.py
@@ -14,57 +14,70 @@ import shutil
 from tabulate import tabulate
 from cvmblaster.blaster import Blaster
 from cvmcore.cvmcore import cfunc
+from pathlib import Path
+from typing import List, Dict, Optional
+from dataclasses import dataclass
 
+@dataclass
+class Config:
+    DEFAULT_THREADS = 8
+    DEFAULT_MIN_ID = 95
+    DEFAULT_MIN_COV = 90
+    DATABASE_DIR = 'db'
+
+def get_database_path() -> Path:
+    """Return the path to the database directory."""
+    return Path(__file__).parent / Config.DATABASE_DIR
 
 def args_parse():
-    "Parse the input argument, use '-h' for help."
     parser = argparse.ArgumentParser(
-        usage='cvmcgmlst -i <genome assemble directory> -o <output_directory> \n\nAuthor: Qingpo Cui(SZQ Lab, China Agricultural University)\n')
+        description='cgMLST analysis tool',
+        usage='cvmcgmlst -i <genome assemble directory> -o <output_directory>'
+    )
+    
+    subparsers = parser.add_subparsers(dest='subcommand')
+    
+    # Simplified subparser creation
+    subparsers.add_parser('show_db', help="Show available databases")
+    subparsers.add_parser('init', help='Initialize reference database')
+    
+    create_parser = subparsers.add_parser('create_db')
+    create_parser.add_argument('-file', required=True, help='Fasta format reference file')
+    create_parser.add_argument('-name', required=True, help='Database name')
+    create_parser.add_argument('-force', action="store_true", help='Force create database')
 
-    # Add subcommand
-    subparsers = parser.add_subparsers(
-        dest='subcommand', title='cvmcgmlst subcommand')
-    show_database_parser = subparsers.add_parser(
-        'show_db', help="<show the list of all available database>")
-
-    init_db_parser = subparsers.add_parser(
-        'init', help='<initialize the reference database>')
-
-    add_db_parser = subparsers.add_parser(
-        'create_db', help='<add custome database, use cvmcgmlst createdb -h for help>')
-
-    add_db_parser.add_argument(
-        '-file', help='<The fasta format reference file>')
-    add_db_parser.add_argument(
-        '-name', help="<The database name parameter that could be used in 'cvmcgmlst -db [name]'>")
-    add_db_parser.add_argument(
-        '-force', default=False, action="store_true", help='<force create database>')
-
-    parser.add_argument(
-        "-i", help="<input_file>: the PATH of assembled genome file")
-    parser.add_argument('-db', help="<database_path>: path of cgMLST database")
-    parser.add_argument("-o", help="<output_directory>: output PATH")
-    parser.add_argument('-minid', default=95,
-                        help="<minimum threshold of identity>, default=95")
-    parser.add_argument('-mincov', default=90,
-                        help="<minimum threshold of coverage>, default=90")
-    parser.add_argument(
-        '-t', default=8, help='<number of threads>: default=8')
+    # Main parser arguments
+    parser.add_argument("-i", help="Input genome assembly file path")
+    parser.add_argument('-db', help="cgMLST database name")
+    parser.add_argument("-o", help="Output directory path")
+    parser.add_argument('-minid', type=float, default=Config.DEFAULT_MIN_ID,
+                    help=f"Minimum identity threshold (default: {Config.DEFAULT_MIN_ID})")
+    parser.add_argument('-mincov', type=float, default=Config.DEFAULT_MIN_COV,
+                    help=f"Minimum coverage threshold (default: {Config.DEFAULT_MIN_COV})")
+    parser.add_argument('-t', type=int, default=Config.DEFAULT_THREADS,
+                    help=f'Number of threads (default: {Config.DEFAULT_THREADS})')
     parser.add_argument('-v', '--version', action='version',
                         version='Version: ' + get_version("__init__.py"), help='Display version')
+    
+    args = parser.parse_args()
     if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
+        parser.print_help()
         sys.exit(1)
-    return parser.parse_args()
-
+    return args
 
 def read(rel_path: str) -> str:
-    here = os.path.abspath(os.path.dirname(__file__))
+    """Read file content from a relative path using pathlib.
+    
+    Args:
+        rel_path: Relative path to the file
+    
+    Returns:
+        The content of the file as a string
+    """
+    here = Path(__file__).parent.resolve()
     # intentionally *not* adding an encoding option to open, See:
     #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with open(os.path.join(here, rel_path)) as fp:
-        return fp.read()
-
+    return (here / rel_path).read_text()
 
 def get_rel_path():
     """
@@ -73,14 +86,12 @@ def get_rel_path():
     here = os.path.abspath(os.path.dirname(__file__))
     return here
 
-
 def get_version(rel_path: str) -> str:
     for line in read(rel_path).splitlines():
         if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     raise RuntimeError("Unable to find version string.")
-
 
 def initialize_db():
     database_path = os.path.join(
@@ -107,37 +118,21 @@ def initialize_db():
         print('No valid reference file exist...')
         sys.exit(1)
 
-
-def create_db(fasta_file, db_name, force=False):
-    database_path = os.path.join(
-        os.path.dirname(__file__), f'db')
-    # 定义源文件路径和目标路径
-    dbfile = f'{db_name}.fsa'
-    dest_file = os.path.join(database_path, dbfile)
-
-    if dbfile not in os.listdir(database_path):
-        # dest_file = os.path.join(database_path, fname)
-        shutil.copy(fasta_file, dest_file)
-        blastdb_out = os.path.join(database_path, db_name)
-        print(f"Add {db_name} to database...")
-        # print(blastdb_out)
-        Blaster.makeblastdb(dest_file,  blastdb_out, db_type='nucl')
-    else:
-        if force:
-            os.remove(dest_file)
-            shutil.copy(fasta_file, dest_file)
-            blastdb_out = os.path.join(database_path, db_name)
-            print(f"Add {db_name} to database...")
-            # print(blastdb_out)
-            Blaster.makeblastdb(dest_file,  blastdb_out, db_type='nucl')
-        else:
-            print(
-                f"{db_name} already exist in database, Please make sure or give another db_name")
-            sys.exit(1)
-    # else:
-    #     print("Wrong suffix with input fasta file")
-    #     sys.exit(1)
-
+def create_db(fasta_file: Path, db_name: str, force: bool = False) -> None:
+    """Create a new database from a fasta file."""
+    database_path = get_database_path()
+    dest_file = database_path / f'{db_name}.fsa'
+    
+    if dest_file.exists() and not force:
+        raise ValueError(f"Database {db_name} already exists. Use -force to overwrite.")
+    
+    if dest_file.exists() and force:
+        dest_file.unlink()
+    
+    shutil.copy(str(fasta_file), str(dest_file))
+    blastdb_out = database_path / db_name
+    print(f"Adding {db_name} to database...")
+    Blaster.makeblastdb(str(dest_file), str(blastdb_out), db_type='nucl')
 
 def check_db():
     """
@@ -151,7 +146,6 @@ def check_db():
             db_name = os.path.splitext(file)[0]
             db_list.append(db_name)
     return db_list
-
 
 def show_db_list():
     """
@@ -188,106 +182,61 @@ def show_db_list():
     tidy_db_df = tabulate(db_df, headers='keys', showindex=False)
     return print(tidy_db_df)
 
+def process_genome(input_file: Path, database: str, output_dir: Path, 
+                  threads: int, min_id: float, min_cov: float) -> None:
+    """Process a single genome file."""
+    if not input_file.exists():
+        raise FileNotFoundError(f"Input file not found: {input_file}")
+    
+    if not cfunc.is_fasta(str(input_file)):
+        raise ValueError(f"Input file is not in FASTA format: {input_file}")
+    
+    output_file = output_dir / f"{input_file.stem}_tab.txt"
+    database_path = get_database_path() / database
+    
+    print(f'Processing {input_file}')
+    result = Blaster(str(input_file), str(database_path),
+                     str(output_dir), threads, min_id, min_cov).mlst_blast()
+    
+    df = pd.DataFrame.from_dict(result, orient='index') if result else pd.DataFrame(columns=['Allele_Num'])
+    df.index.name = 'Loci'
+    df.to_csv(output_file, sep='\t', index=True)
+    print(f"Results written to {output_file}")
 
 def main():
-    # df_all = pd.DataFrame()
     args = args_parse()
+    
     if args.subcommand is None:
-        # print('Start cgMLST analysis ...')
-        # threads
-        threads = args.t
-        # print(threads)
-
-        minid = args.minid
-        mincov = args.mincov
-
-        # check if the output directory exists
-        if not os.path.exists(args.o):
-            os.mkdir(args.o)
-        output_path = os.path.abspath(args.o)
-        # print(output_path)
-
-        # get the database path
-        database = args.db
-
-        # check if input db is in dblist
-        exist_database = check_db()
-
-        if database in exist_database:
-            database_path = os.path.join(
-                os.path.dirname(__file__), f'db/{args.db}')
-            # seq_type = cfunc.check_sequence_type(f'{database_path}.fsa')
-        else:
-            print(
-                f'Could not found {database} in {exist_database}, Please check your input or view database list using "cvmcgmlst show_db"')
+        output_dir = Path(args.o)
+        output_dir.mkdir(exist_ok=True)
+        
+        if args.db not in check_db():
+            print(f"Database {args.db} not found. Use 'cvmcgmlst show_db' to list available databases.")
             sys.exit(1)
-
-        # decide blast type
-        # print(f'The database type is {seq_type} \n')
-        # if seq_type == 'Amino Acid':
-        #     blast_type = 'blastx'
-        # else:
-        #     blast_type = 'blastn'
-
-        # get the input assembled genome path
-        inputfile = os.path.abspath(args.i)
-        file_base = str(os.path.basename(os.path.splitext(inputfile)[0]))
-        output_filename = file_base + '_tab.txt'
-
-        # Create the outfile
-        outfile = os.path.join(output_path, output_filename)
-
-        # print(file_path)
-        if os.path.isfile(inputfile):
-            # print("TRUE")
-            if cfunc.is_fasta(inputfile):
-                print(f'Processing {inputfile}')
-                # cvmcgmlst is blastn, so here I ignore the blast_type parameter in mlst_blast
-                result = Blaster(inputfile, database_path,
-                                 output_path, threads, minid, mincov).mlst_blast()
-
-                # print(df)
-                if len(result) != 0:
-                    df = pd.DataFrame.from_dict(result, orient='index')
-                    df.index.name = 'Loci'
-                    df.rename(columns={0: 'Allele_Num'}, inplace=True)
-                    df_trans = df.T
-                    df_trans.index = [file_base]
-
-                    # df_out['FILE'] = file_base
-                    # order = list(reversed(df.columns.to_list()))
-                    # df = df[order]
-                    # print(df)
-                    df.to_csv(outfile, sep='\t', index=True)
-                    print(
-                        f"Finishing process {inputfile}: writing results to " + str(outfile))
-                else:
-                    print('Empty result')
-                    df = pd.DataFrame(columns=['Loci', 'Allele_Num'])
-                    df.to_csv(outfile, sep='\t')
-                    # print(df)
-                #     df.index.name = 'Loci'
-                #     df_trans = df.T
-                #     df_trans.index = [file_base]
-                # df_all = pd.concat([df_all, df_trans])
-        else:
-            print('Could not find your input file, exit...')
-    elif(args.subcommand == 'show_db'):
+            
+        try:
+            process_genome(
+                input_file=Path(args.i),
+                database=args.db,
+                output_dir=output_dir,
+                threads=args.t,
+                min_id=args.minid,
+                min_cov=args.mincov
+            )
+        except Exception as e:
+            print(f"Error processing genome: {e}")
+            sys.exit(1)
+    
+    elif args.subcommand == 'show_db':
         show_db_list()
-    elif(args.subcommand == 'init'):
+    elif args.subcommand == 'init':
         initialize_db()
-    elif(args.subcommand == 'create_db'):
-        custome_db_file = os.path.abspath(args.file)
-        databasename = args.name
-        force = args.force
-        create_db(custome_db_file, databasename, force)
-        print(f'Adding {args.file} to reference database...')
-        # print(f'Initializing reference data...')
-        # initialize_db()
-    else:
-        print(
-            f'{args.subcommand} do not exists, please using "ResBlaster -h" to show help massage.')
-
+    elif args.subcommand == 'create_db':
+        try:
+            create_db(Path(args.file), args.name, args.force)
+        except Exception as e:
+            print(f"Error creating database: {e}")
+            sys.exit(1)
 
 if __name__ == '__main__':
     main()
